38198dfda7acd4cacb593f3e10040beff89c62f1
Satyendra Kumar <satyen@us.ibm.com>
2021-05-11 23:27:34 -0500

    finalize styling

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {Link} from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import numeral from 'numeral';
 import getExpensesTotal from '../selectors/expenses-total';
 
 
 const ExpensesSummary = ({expenseCount, expensesTotal}) => {
-  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
-  const formattedExpensesTotal = numeral(expensesTotal/100).format('$0,0.00');
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal/100).format('$0,0.00');
+    
     return (
-        <div>
-            <h1>Viewing {expenseCount} {expenseWord} totaling {formattedExpensesTotal}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">
+                    Viewing 
+                    <span> {expenseCount}</span> {expenseWord} totaling <span> {formattedExpensesTotal}</span>
+                    <div className="page-header__actions">
+                        <Link className="button" to="/create">Add Expense</Link>
+                    </div>
+                </h1>
+            </div>
         </div>
     );
 };
